eed17d43d274154cae23fc2443d910f11a8013e7
Kanel04 <thelegendanelka@gmail.com>
2022-09-27 17:01:46 +0300

    iframe load

--- src/pages/Primary/components/Iframe/index.jsx
+++ src/pages/Primary/components/Iframe/index.jsx
@@ -1,40 +1,47 @@
-import React from 'react';
+import {useRef, useEffect} from 'react';
 
 function IframeComponent() {
-/*   const componentText = document.querySelector("#component-text");
-const componentContainer = document.querySelector("#components_list");
+  const ref = useRef(null);
 
-const loadComponents = () => {
-  componentText.textContent = "Loading components...";
-  fetch("/components")
-    .then((res) => res.json())
-    .then((data) => {
-      for (item of data) {
-        appendComponentToList(item.components);
-      }
-    })
-    .catch((error) => alert(error))
-    .finally(() => (componentText.textContent = "Components list"));
-};
-
-const appendComponentToList = (data) => {
-  const script = document.createElement("script");
-  script.text = data.content;
-  document.head.appendChild(script);
-  const liComponent = document.createElement("li");
-  liComponent.classList.add("component", "cursor-pointer");
-  const component = document.createElement(data.name);
-  liComponent.appendChild(component); 
-  componentContainer.appendChild(liComponent);
-};
-
-loadComponents();
-
- */
+ 
+  const loadComponents = () => {
+    useEffect(() => {
+      // 👇️ use a ref (best)
+      const componentText = ref.current;
+      console.log(componentText);
+      const componentContainer = document.getElementById("components_list");
+      console.log(componentContainer);
+      componentText.textContent = "Loading components...";
+      fetch("/components")
+        .then((res) => res.json())
+        .then((data) => {
+          for (item of data) {
+            appendComponentToList(item.components);
+          }
+        })
+        .catch((error) => alert(error))
+        .finally(() => (componentText.textContent = "Components list"));
+        const appendComponentToList = (data) => {
+          const script = document.createElement("script");
+          script.text = data.content;
+          document.head.appendChild(script);
+          const liComponent = document.createElement("li");
+          liComponent.classList.add("component", "cursor-pointer");
+          const component = document.createElement(data.name);
+          liComponent.appendChild(component); 
+          componentContainer.appendChild(liComponent);
+        };
+    
+    }, []);
+    
+  };
+   
+  loadComponents();
+  
   return (
     <div>
-        <h1 id="component-text"   className="font-bold">Components List</h1>
-        <ul id="components_list"   className="my-4 grid grid-cols-1 gap-8">
+        <h1 id="component-text" ref={ref}  className="font-bold">Components List</h1>
+        <ul id="components_list" onLoad={loadComponents}  className="my-4 grid grid-cols-1 gap-8">
                     
         </ul>
     </div>
